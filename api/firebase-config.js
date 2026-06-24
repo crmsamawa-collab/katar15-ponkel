84c232721886d529090db0fa4e8b3b9445d49e0a
crmsamawa-collab <crmsamawa@gmail.com>
2026-06-25 01:49:57 +0700

    firebase-config.js

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -4,14 +4,14 @@ module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-    res.style = "Content-Type", "application/json";
+    res.setHeader('Content-Type', 'application/json');
 
     // Kirim konfigurasi Firebase dari Environment Variables Vercel
     res.status(200).json({
         apiKey: process.env.FIREBASE_API_KEY,
         authDomain: process.env.FIREBASE_AUTH_DOMAIN,
         projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: typeof process.env.FIREBASE_STORAGE_BUCKET !== 'undefined' ? process.env.FIREBASE_STORAGE_BODY : "",
+        storageBucket: typeof process.env.FIREBASE_STORAGE_BUCKET !== 'undefined' ? process.env.FIREBASE_STORAGE_BUCKET : "",
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID
     });
